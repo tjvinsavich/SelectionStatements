3d0393a9d86cf41532c4d060e614c2bd63458300
TJ Vinsavich <tjvinsavich@gmail.com>
2021-06-11 14:25:27 -0500

    initial commit

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            Console.WriteLine("Guess my favorite number");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high");
+            }
+            else if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind."); 
+            }
+        }
+    }
+}
